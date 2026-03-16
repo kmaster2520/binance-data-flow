4e246aaf9d79f389dec66b6a82fcfabd991c29f7
kmaster2520 <sathvik.kadaveru@gmail.com>
2026-03-16 17:19:42 -0400

    combine endpoints into sg cft

--- endpoints/create_security_groups_endpoints.sh
+++ endpoints/create_security_groups_endpoints.sh
@@ -4,8 +4,8 @@ VPC_ID="vpc-013914d1062557cd7"
 SUBNET_ID="subnet-0ac0bc58b5403af7b"
 
 aws cloudformation create-stack \
-  --stack-name BinanceSecurityGroups \
-  --template-body file://btc-websocket-sg.yaml \
+  --stack-name CoinbaseSecurityGroups \
+  --template-body file://btc-websocket-endpoints.yaml \
   --parameters \
       ParameterKey=VpcId,ParameterValue=$VPC_ID \
-      ParameterKey=InstanceConnectSubnetId,ParameterValue=$SUBNET_ID
+      ParameterKey=SubnetId,ParameterValue=$SUBNET_ID
